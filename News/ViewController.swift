916e10b544549df4e5a4eae4f7a4e98d3b382bff
Katty57 <kate.tyan57@gmail.com>
2022-02-07 16:07:20 +0600

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by  User on 07.02.2022.
+//  Copyright © 2022 abr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
